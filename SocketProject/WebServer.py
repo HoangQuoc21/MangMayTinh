b49651d0ebcfda6beb2a69f807a5f7baee9a5e36
HoangQuoc21 <hoangquoc2106@gmail.com>
2022-11-09 07:27:25 +0700

    [Quốc]: Sửa http header và thêm ghi chú mục

--- SocketProject/WebServer.py
+++ SocketProject/WebServer.py
@@ -22,19 +22,26 @@ try:
 except socket.error as e:
     print(f'Socket error: {e}\n')
     print('Socket error: %s\n' %(e))
+    
+def http_header(header_type):
+    if header_type == '200':
+        header = 'HTTP/1.1 200 OK\n'
+    elif header_type == '404':
+        header = 'HTTP/1.0 404 page not found\n'
+    return header
 
 #Hàm trả về response_header tương ứng với từng loại content_type
-def response_header(Content_type):
-    message_header = 'HTTP/1.1 200 \n'
+def response_header(header_type, Content_type):
+    message_header = http_header(header_type)
     message_header += f'Content-type: {Content_type}'
     message_header += '\r\n\r\n'
     message_header = message_header.encode()
     return message_header
 
 #Hàm đọc nội dung file và chuyển data đã đọc về dưới dạng nhị phân (byte)
-def read_file(file_url, Content_type):
+def read_file(file_url, header_type, Content_type):
     f = open(file_url, 'rb')
-    f_data = response_header(Content_type)
+    f_data = response_header(header_type, Content_type)
     f_data += f.read()
     return f_data
 
@@ -57,22 +64,26 @@ def handle(client, addr):
         print(f'-Request method: {request_method}')
         print(f'-Request url: {request_url}\n')
         
+        # == 3. TẢI ĐƯỢC PAGE INDEX.HTML == 
         #Nếu method nhận được là GET: 
         if request_method == 'GET':
             if request_url == '/' or request_url == '/index.html':
                 url = 'D:/Github/MangMayTinh/SocketProject/index.html'
                 Content_type = 'text/html'
+                header_type = '200'
                 #SendBackData (nhị phân) là dữ liệu đọc từ hàm read_file (bao gồm response_header và nội dung file đọc tương ứng)   
-                sendBackData = read_file(url, Content_type)
+                sendBackData = read_file(url,header_type, Content_type)
             elif request_url == '/css/style.css':
                 url = 'D:/Github/MangMayTinh/SocketProject/css/style.css'
                 Content_type = 'text/css'
+                header_type = '200'
                 #SendBackData (nhị phân) là dữ liệu đọc từ hàm read_file (bao gồm response_header và nội dung file đọc tương ứng)   
-                sendBackData = read_file(url, Content_type)
+                sendBackData = read_file(url,header_type, Content_type)
             elif request_url == '/favicon.ico':
                 url = 'D:/Github/MangMayTinh/SocketProject/favicon.ico'
                 Content_type = 'image/x-icon'
-                sendBackData = read_file(url, Content_type)
+                header_type = '200'
+                sendBackData = read_file(url,header_type, Content_type)
             elif(request_url.split('/')[1] == 'avatars'): 
                 if request_url == '/avatars/1.png':
                     url = 'D:/Github/MangMayTinh/SocketProject/avatars/1.png'
@@ -90,14 +101,17 @@ def handle(client, addr):
                     url = 'D:/Github/MangMayTinh/SocketProject/avatars/7.png'
                 elif request_url == '/avatars/8.png':
                     url = 'D:/Github/MangMayTinh/SocketProject/avatars/8.png'
+                header_type = '200'
                 Content_type = 'image/png'
                 #SendBackData (nhị phân) là dữ liệu đọc từ hàm read_file (bao gồm response_header và nội dung file đọc tương ứng)   
-                sendBackData = read_file(url, Content_type)
+                sendBackData = read_file(url,header_type, Content_type)
             else:
+                # == 4. LỖI PAGE ==
                 #Nếu Load Page Không Đúng Thì Trả Về 404.html
+                header_type = '404'
                 url = 'D:/Github/MangMayTinh/SocketProject/404.html'
                 Content_type = 'text/html'
-                sendBackData = read_file(url, Content_type)
+                sendBackData = read_file(url,header_type, Content_type)
                 
             #Gửi nội dung data đã đọc lại cho client
             client.send(sendBackData)
@@ -109,12 +123,16 @@ def handle(client, addr):
         client.close()
         break
 
+# == 1. KẾT NỐI == 
 #Cho Mở Kết Nối Server Để Lắng Nghe Kết Nối Từ Client
 def start_server():
     SERVER.listen()
     while True:
+        # == 6. MULTIPLE REQUESTS
         #Chấp nhận Kết Nối từ Client 
         conn, addr = SERVER.accept()
+        
+        # == 7. MULTIPLE CONNECTIONS ==
         #Thread dùng để xử lý nhiều connection cùng 1 lúc
         thread = threading.Thread(target= handle, args=(conn, addr))
         thread.start()
